a554654c33d428fc4e43292575426232b14dba82
Jesus Perez <ios.developer.jesus@gmail.com>
2025-05-08 10:56:05 -0600

    Initial commit of project files

---
+++ pruebaTecnicaLiverpool/pruebaTecnicaLiverpool/pruebaTecnicaLiverpoolApp.swift
@@ -0,0 +1,17 @@
+//
+//  pruebaTecnicaLiverpoolApp.swift
+//  pruebaTecnicaLiverpool
+//
+//  Created by Jesus Perez on 08/05/25.
+//
+
+import SwiftUI
+
+@main
+struct pruebaTecnicaLiverpoolApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
